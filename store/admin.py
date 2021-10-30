02f18cd4d6acdadeb9c4a2ea6b47e30abbed87ad
basil <basilsajeevthevarmadam@gmail.com>
2021-10-30 22:12:17 +0530

    revamp

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','category','price','stock','is_available','modified_date')
     prepopulated_fields={
         'slug':('product_name',)
     }
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','variation_category','variation_value','is_active')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category','variation_value','is_active')
+
 
 admin.site.register(Product,ProductAdmin) 
-# Register your models here.
+admin.site.register(Variation,VariationAdmin) 
